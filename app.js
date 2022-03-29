21783e638059980c567c8723edb9c620953a73b8
Anthony Tran <hello@anthonytran.me>
2022-03-29 16:42:56 -0700

    added MongoDB connection

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
 
 app.use(cors());
 app.use(express.json());
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error.message);
+  });
 
 module.exports = app;
