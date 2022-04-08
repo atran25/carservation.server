90744592021b1d2426b457060ca9ab50d94b3b14
Anthony Tran <hello@anthonytran.me>
2022-04-07 19:15:58 -0700

    changed serviceAccountKey to google-credentials to work with heroku

--- authenticateToken.js
+++ authenticateToken.js
@@ -1,23 +1,27 @@
 // authenticateToken.js
-const admin = require('firebase-admin');
-var serviceAccount = require("./serviceAccountKey.json");
+const admin = require("firebase-admin");
+var serviceAccount = require("./google-credentials.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://Car-eservation.firebaseio.com',
-  });
-  
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://Car-eservation.firebaseio.com",
+});
+
 async function decodeIDToken(request, res, next) {
   const header = request.headers?.authorization;
-      if (header !== 'Bearer null' && request.headers?.authorization?.startsWith('Bearer ')) {const idToken = request.headers.authorization.split('Bearer ')[1];
-      try {
-          console.log("idtoken: ", idToken)
-          const decodedToken = await admin.auth().verifyIdToken(idToken);
-          request['currentUser'] = true;
-        } catch (err) {
-          console.log(err);
-        }
-      }
+  if (
+    header !== "Bearer null" &&
+    request.headers?.authorization?.startsWith("Bearer ")
+  ) {
+    const idToken = request.headers.authorization.split("Bearer ")[1];
+    try {
+      console.log("idtoken: ", idToken);
+      const decodedToken = await admin.auth().verifyIdToken(idToken);
+      request["currentUser"] = true;
+    } catch (err) {
+      console.log(err);
+    }
+  }
   next();
 }
 module.exports = decodeIDToken;
